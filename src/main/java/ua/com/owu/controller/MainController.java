e27226288be01f11e5dd165e1beabd58301bc5fa
4iki93@gmail.com <love0412>
2017-10-24 13:47:01 +0300

    good

--- src/main/java/ua/com/owu/controller/MainController.java
+++ src/main/java/ua/com/owu/controller/MainController.java
@@ -2,41 +2,34 @@ package ua.com.owu.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import ua.com.owu.entity.User;
 import ua.com.owu.service.UserService;
 
 @Controller
 public class MainController {
-
-
     @Autowired
     private UserService userService;
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String  home(){
+    public String home() {
 
         return "index";
     }
 
-
     @RequestMapping(value = "/users", method = RequestMethod.GET)
-    public String  users(){
+//    @GetMapping()
+    public String users() {
 
         return "users";
     }
 
-
-    @PostMapping("/seve")
-    public String saveUser(@RequestParam("name") String name,@RequestParam String surname){
+    @PostMapping("/save")
+    public String saveUser(@RequestParam("name") String name, @RequestParam String surname) {
         User user = new User();
-        user.setName(name);
         user.setName(name);
         user.setSurname(surname);
         userService.save(user);
         return "index";
     }
-
-
 }
