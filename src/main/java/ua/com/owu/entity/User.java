9c2a6e90c9243c3065a55d194802ea2ff89ce894
4iki93@gmail.com <love0412>
2017-10-24 12:50:35 +0300

    Initial commit

---
+++ src/main/java/ua/com/owu/entity/User.java
@@ -0,0 +1,25 @@
+package ua.com.owu.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@ToString
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String surname;
+}
